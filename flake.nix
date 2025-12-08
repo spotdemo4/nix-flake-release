3537df07a99e6805a68562f6b9a2630e28ced455
trev <me@trev.xyz>
2025-12-08 07:36:55 -0500

    fix: independent action check

--- flake.nix
+++ flake.nix
@@ -94,6 +94,16 @@
             '';
           };
 
+          action = {
+            src = ./.;
+            deps = with pkgs; [
+              action-validator
+            ];
+            script = ''
+              action-validator action.yaml
+            '';
+          };
+
           nix = {
             src = ./.;
             deps = with pkgs; [
